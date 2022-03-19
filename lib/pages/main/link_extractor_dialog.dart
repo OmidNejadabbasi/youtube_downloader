ee6750826a27d12aacbce7ab72eb6252dac2fad5
Omid Nejadabbasi <nejadabbasio@gmail.com>
2022-03-19 12:47:12 +0330

    add dialog action buttons to the dialog

--- lib/pages/main/link_extractor_dialog.dart
+++ lib/pages/main/link_extractor_dialog.dart
@@ -22,25 +22,54 @@ class _YoutubeLinkExtractorDialogState
   Widget build(BuildContext context) {
     return Dialog(
       child: Container(
-        padding: const EdgeInsets.all(14),
+        padding: const EdgeInsets.only(left: 14, right: 14, top: 14, bottom: 4),
         child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: const [
-            Text('Link', style: Styles.labelTextStyle,),
-            NIconButton(
-              icon: CustomIcons.clipboard,
-              iconSize: 20,
-            ),
-          ]),
-          const SizedBox(height: 12,),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Link',
+                    style: Styles.labelTextStyle,
+                  ),
+                  NIconButton(
+                    icon: CustomIcons.clipboard,
+                    iconSize: 20,
+                  ),
+                ]),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
           TextFormField(
             controller: _linkInputController,
-
             decoration: const InputDecoration(
               border: OutlineInputBorder(),
-              labelText: 'Paste...',
+              labelText: 'Video Link or Id…',
               labelStyle: Styles.labelTextStyle,
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, null);
+              },
+              child: const Text(
+                'Cancel',
+                style: Styles.labelTextStyle,
+              ),
+            ),
+            const TextButton(
+              onPressed: null,
+              child: Text(
+                'Start',
+                style: Styles.labelTextStyle,
+              ),
+            )
+          ],),
         ]),
       ),
     );
